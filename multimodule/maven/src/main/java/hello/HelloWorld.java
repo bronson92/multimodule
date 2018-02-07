b391834d637d6ba10e48f50ca9dc8b431120169d
bronson92 <krav4ukoo@gmail.com>
2018-02-07 20:49:25 +0200

    first commit

---
+++ multimodule/maven/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.epam.training.hello;
+
+public class HelloWorld {
+   public static void main(String[] args) throws Exception {
+       System.out.println("Hello World!!!!");
+   }
+}
---
+++ multimodule/module/src/main/java/module/HelloVasia.java
@@ -0,0 +1,7 @@
+package com.epam.training;
+
+public class HelloVasia {
+   public static void main(String[] args) throws Exception {
+       System.out.println("Hello Vasia!!!!");
+   }
+}
